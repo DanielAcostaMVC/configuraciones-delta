1d864e22bef797924e82b08cd38e9b82eea81d09
Daniel Acosta <daniel.acosta@sinco.com.co>
2024-05-03 15:25:57 -0500

    🧹configuracion-empresa

--- app/empresa/layout.tsx
+++ app/empresa/layout.tsx
@@ -1,6 +1,20 @@
 'use client';
 
-import { ConfiguracionEmpresa } from '../ui/empresa/configuracion-empresa';
+import { Container, Grid /*, Stack*/ } from '@mui/material';
+
+import {
+  Auditoria,
+ /* CardFirma,
+  CardRepresentanteLegal,
+  DatosEmpresa,
+  FooterActions,*/
+  LogoEmpresa,
+  NombreEmpresa,
+//  SkeletonEmpresa,
+} from '../ui/empresa';
+
+
+
 import {
   BoxCustomView, 
  /* EmptyStateError,
@@ -12,11 +26,42 @@ const { BoxCustomScroll } = BoxEstilos();
 
 export default function LayoutEmpresa  ({ children }: { children: React.ReactNode })  {
   return (
-    <BoxCustomView>
-      <Box>
-      <ConfiguracionEmpresa children = {children} />
+    <>
+      <BoxCustomView>
+      {/* <PageHeader title="Configuración de la Empresa" />*/}
       
-      </Box>
+
+      <Container maxWidth="md">
+        <Grid container justifyContent="center" alignItems="center">
+          <Grid item xs={12} md={12} lg={12}>
+            <NombreEmpresa razonSocial={'RazonSocial'} nit={'NitXz'} />
+          </Grid>
+          <Grid container justifyContent="center">
+            <Grid item xs={2} sm={2} md={2} lg={2}>
+              <LogoEmpresa logo={"/logo-empresa.png"} />
+            </Grid>
+            <Grid item xs={10} sm={10} md={10} lg={10}>
+              {children}
+              {/*<BoxCustomScroll>
+                <Stack gap={1.5}>
+                  <CardRepresentanteLegal Representante={RepresentanteLegal} />
+                  <CardFirma Firma={RepresentanteLegal.Firma} />
+                  <DatosEmpresa
+                    Direccion={Direccion}
+                    ConstitucionEmpresa={ConstitucionEmpresa}
+                    Ciudad={Ciudad}
+                    Poliza={Poliza}
+                  />
+                </Stack>
+  </BoxCustomScroll>*/}
+              <Auditoria UltimaModificacion={{Fecha:'aQuemado', Usuario:'bQuemado'}} />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
+
+      {/*isActiveFooter && <FooterActions />*/}
     </BoxCustomView>
+    </>
   );
 };
