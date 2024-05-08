a7b9a0bd3b62be6b9ceeb01136bacb06344034cd
Daniel Acosta <daniel.acosta@sinco.com.co>
2024-05-08 18:51:35 -0500

    fix build navega representante + firmaX

--- app/empresa/edit/page.tsx
+++ app/empresa/edit/page.tsx
@@ -72,7 +72,7 @@ export default async function PageEditRepresentanteLegal() {
     >
         
       
-        <FormRepresentanteLegal RepresentanteLegal={RepresentanteLegal} />
+        <FormRepresentanteLegal />
       
     </CardCustom>
   );
